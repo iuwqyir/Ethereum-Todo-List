dda57cbd6466efd3e91071185ea85479d5b93ef3
Iuwqyir <toomas.oosalu@gmail.com>
2019-08-11 00:40:08 +0100

    ADD event to log todo item creation

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -13,12 +13,20 @@ contract TodoList {
 	uint public todoItemCount = 0;
 	mapping (uint => TodoItem) public todoItems;
 
+	event TaskCreated(
+		uint id,
+		string title,
+		string content,
+		bool isCompleted
+	);
+
 	constructor() public {
 		createTodoItem("MyFirstTodoItem", "Do it fast!");
 	}
 
 	function createTodoItem(string memory _title, string memory _content) public {
 		todoItems[todoItemCount] = TodoItem(todoItemCount, _title, _content, false);
+		emit TaskCreated(todoItemCount, _title, _content, false);
 		todoItemCount++;
 	}
 }
